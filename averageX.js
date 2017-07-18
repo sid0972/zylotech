d8e0323ed7f43afb3869ab390f45254b5fbe5c42
Siddhartha Srivastava <sid7513@gmail.com>
2017-07-19 00:40:38 +0530

    Update averageX.js

--- averageX.js
+++ averageX.js
@@ -3,9 +3,12 @@ emit(this.userID, this.X);
 };
 
 var reduceFunction1 = function(userID, X) {
-return Array.sum(X)/this.X;
+total=0
+for (var i=0;i<X.count();i++){
+    total+=1
+return Array.sum(X)/total;
+}
 };
-
 db.transaction.mapReduce(
 mapfunction1,
 reduceFunction1,
